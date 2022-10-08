da71881f5f35f55cee0a8b70ab770c2916fdfd8d
shenxin <wb_shenxin@meituan.com>
2022-10-08 19:47:59 +0800

    安装 react-router 6.4

--- main.tsx
+++ main.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello world!少时诵诗书所所</div>,
+    },
+]);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
 );
